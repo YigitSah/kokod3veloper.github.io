aa4f90a1f22dfabd34dfc67dd6870fc7bb14f063
KokoPolis <63654629+KokoD3veloper@users.noreply.github.com>
2021-11-03 17:40:34 +0200

    Update faraway.js

--- faraway.js
+++ faraway.js
@@ -1,10 +1,17 @@
-function showToast() {
-    // Get the snackbar DIV
-    var x = document.getElementById("snackbar");
+
+class BrApi {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+
+  }
+  showNotification(text,options,time = 3000) {
+    var x = document.getElementById("notifbar");
   
     // Add the "show" class to DIV
     x.className = "show";
   
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, time);
   }
+}
