959e560d70215bfaeb8b5c4f5c2ab2f76153405b
KokoPolis <63654629+KokoD3veloper@users.noreply.github.com>
2021-11-03 20:20:15 +0200

    Update faraway.js

--- faraway.js
+++ faraway.js
@@ -1,12 +1,12 @@
 class BrApi {
 
-  static showNotification(text,options,time = 3000) {
+  static showNotification(text,options = "red" ,time = 3000) {
     var x = document.getElementById("notifbar");
   
     // Add the "show" class to DIV
     x.className = "show";
   
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, time);
+    setTimeout(function(){ x.className = x.className.replace("show", ""); x.innerHTML = text; x.style.background = options; }, time);
   }
 }
